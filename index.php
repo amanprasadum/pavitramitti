5cc5c7c887b2591afdee3c196a718295ebadfb7f
AmanPrasad <amanprasad.um@gmail.com>
2026-04-18 13:25:24 +0530

    sdf

--- index.php
+++ index.php
@@ -7,11 +7,14 @@
     <title>Pavitra Mitti Plant Nursery in Basirhat | Best Nursery Near Barasat & Kolkata</title>
     <meta name="description"
         content="Pavitra Mitti Nursery – 200+ plant varieties in Basirhat, near Barasat & Kolkata. Fruit plants, flowering plants, medicinal herbs, ornamental & landscape plants. Bulk orders welcome.">
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5B3ZBD6B');</script>
+    <script>
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5B3ZBD6B');
+    </script>
+
  <?php include 'common/header-script.php'; ?>
    
 </head>
