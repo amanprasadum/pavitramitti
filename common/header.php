8887d64b5485c20fbca8cab58f40e2e7500eeab0
AmanPrasad <amanprasad.um@gmail.com>
2026-04-20 19:24:04 +0530

    sfg

--- common/header.php
+++ common/header.php
@@ -1,129 +1,69 @@
+<!-- ==================== NAVBAR ==================== -->
+<nav class="navbar navbar-expand-lg fixed-top">
+    <div class="container">
+        <a class="navbar-brand-" href="/">
+            <img src="../assets/images/logo.png" alt="Pavitra Mitti Logo" class="brand-logo-" style="width: 100px;">
+        </a>
 
-    <!-- ==================== NAVBAR ==================== -->
-    <nav class="navbar navbar-expand-lg fixed-top">
-        <div class="container">
-            <a class="navbar-brand-" href="/">
-                <!-- <div class="brand-leaf"><i class="fas fa-leaf"></i></div> -->
-                <!-- Pavitra Mitti  -->
-                <!-- add the logo  -->
-                <img src="../assets/images/logo.png" alt="Pavitra Mitti Logo" class="brand-logo-" style="width: 100px;">
-            </a>
+        <button class="navbar-toggler border-0 shadow-none" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navMain" aria-controls="navMain" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-            <button class="navbar-toggler border-0 shadow-none" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navMain" aria-controls="navMain" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+        <div class="collapse navbar-collapse" id="navMain">
+            <ul class="navbar-nav mx-auto gap-0">
+                <li class="nav-item">
+                    <a class="nav-link active" href="/">Home</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">Plants</a>
+                    <ul class="dropdown-menu plants-menu">
+                        <li><a class="dropdown-item" href="/product/aglaonema-snow-white.php"><i class="fas fa-leaf"></i> Aglaonema Snow White</a></li>
+                        <li><a class="dropdown-item" href="/product/aglaonema.php"><i class="fas fa-leaf"></i> Aglaonema</a></li>
+                        <li><a class="dropdown-item" href="/product/allamanda.php"><i class="fas fa-sun"></i> Allamanda</a></li>
+                        <li><a class="dropdown-item" href="/product/aloe-vera.php"><i class="fas fa-mortar-pestle"></i> Aloe Vera</a></li>
+                        <li><a class="dropdown-item" href="/product/bougainvillea.php"><i class="fas fa-spa"></i> Bougainvillea</a></li>
+                        <li><a class="dropdown-item" href="/product/dragon-fruit.php"><i class="fas fa-apple-alt"></i> Dragon Fruit</a></li>
+                        <li><a class="dropdown-item" href="/product/dragon-mulberry.php"><i class="fas fa-apple-alt"></i> Dragon Mulberry</a></li>
+                        <li><a class="dropdown-item" href="/product/exotic-peach-yellow-hibiscus.php"><i class="fas fa-spa"></i> Exotic Peach Yellow Hibiscus</a></li>
+                        <li><a class="dropdown-item" href="/product/frangipani.php"><i class="fas fa-spa"></i> Frangipani</a></li>
+                        <li><a class="dropdown-item" href="/product/grafted-hibiscus.php"><i class="fas fa-spa"></i> Grafted Hibiscus</a></li>
+                        <li><a class="dropdown-item" href="/product/hot-pink-hibiscus.php"><i class="fas fa-spa"></i> Hot Pink Hibiscus</a></li>
+                        <li><a class="dropdown-item" href="/product/jade-plant.php"><i class="fas fa-seedling"></i> Jade Plant</a></li>
+                        <li><a class="dropdown-item" href="/product/light-peach-hibiscus.php"><i class="fas fa-spa"></i> Light Peach Hibiscus</a></li>
+                        <li><a class="dropdown-item" href="/product/money-plant.php"><i class="fas fa-seedling"></i> Money Plant</a></li>
+                        <li><a class="dropdown-item" href="/product/orange-hibiscus.php"><i class="fas fa-spa"></i> Orange Hibiscus</a></li>
+                        <li><a class="dropdown-item" href="/product/peach-yellow-hibiscus.php"><i class="fas fa-spa"></i> Peach Yellow Hibiscus</a></li>
+                        <li><a class="dropdown-item" href="/product/philodendron-birkin.php"><i class="fas fa-leaf"></i> Philodendron Birkin</a></li>
+                        <li><a class="dropdown-item" href="/product/pink-double-petal-hibiscus.php"><i class="fas fa-spa"></i> Pink Double Petal Hibiscus</a></li>
+                        <li><a class="dropdown-item" href="/product/snake-plant.php"><i class="fas fa-seedling"></i> Snake Plant</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">Services</a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="/services/nursery-consultation.php"><i class="fas fa-seedling"></i> Nursery Consultation</a></li>
+                        <li><a class="dropdown-item" href="/services/bulk-orders.php"><i class="fas fa-truck"></i> Bulk Orders & Delivery</a></li>
+                        <li><a class="dropdown-item" href="/services/landscaping.php"><i class="fas fa-drafting-compass"></i> Landscaping Support</a></li>
+                        <li><a class="dropdown-item" href="/services/plant-care-guidance.php"><i class="fas fa-hand-holding-heart"></i> Plant Care Guidance</a></li>
+                        <li><a class="dropdown-item" href="/services/soil-fertility-consultation.php"><i class="fas fa-hand-holding-heart"></i> Soil Fertility Consultation</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
+                <li class="nav-item"><a class="nav-link" href="#faq">FAQ</a></li>
+                <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+            </ul>
 
-            <div class="collapse navbar-collapse" id="navMain">
-                <ul class="navbar-nav mx-auto gap-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="/">Home</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">Plants</a>
-                        <ul class="dropdown-menu plants-menu">
-                            <li><a class="dropdown-item" href="/product/aglaonema-snow-white.php"><i class="fas fa-leaf"></i> Aglaonema Snow White</a></li>
-                            <li><a class="dropdown-item" href="/product/aglaonema.php"><i class="fas fa-leaf"></i> Aglaonema</a></li>
-                            <li><a class="dropdown-item" href="/product/allamanda.php"><i class="fas fa-sun"></i> Allamanda</a></li>
-                            <li><a class="dropdown-item" href="/product/aloe-vera.php"><i class="fas fa-mortar-pestle"></i> Aloe Vera</a></li>
-                            <li><a class="dropdown-item" href="/product/bougainvillea.php"><i class="fas fa-spa"></i> Bougainvillea</a></li>
-                            <li><a class="dropdown-item" href="/product/dragon-fruit.php"><i class="fas fa-apple-alt"></i> Dragon Fruit</a></li>
-                            <li><a class="dropdown-item" href="/product/dragon-mulberry.php"><i class="fas fa-apple-alt"></i> Dragon Mulberry</a></li>
-                            <li><a class="dropdown-item" href="/product/exotic-peach-yellow-hibiscus.php"><i class="fas fa-spa"></i> Exotic Peach Yellow Hibiscus</a></li>
-                            <li><a class="dropdown-item" href="/product/frangipani.php"><i class="fas fa-spa"></i> Frangipani</a></li>
-                            <li><a class="dropdown-item" href="/product/grafted-hibiscus.php"><i class="fas fa-spa"></i> Grafted Hibiscus</a></li>
-                            <li><a class="dropdown-item" href="/product/hot-pink-hibiscus.php"><i class="fas fa-spa"></i> Hot Pink Hibiscus</a></li>
-                            <li><a class="dropdown-item" href="/product/jade-plant.php"><i class="fas fa-seedling"></i> Jade Plant</a></li>
-                            <li><a class="dropdown-item" href="/product/light-peach-hibiscus.php"><i class="fas fa-spa"></i> Light Peach Hibiscus</a></li>
-                            <li><a class="dropdown-item" href="/product/money-plant.php"><i class="fas fa-seedling"></i> Money Plant</a></li>
-                            <li><a class="dropdown-item" href="/product/orange-hibiscus.php"><i class="fas fa-spa"></i> Orange Hibiscus</a></li>
-                            <li><a class="dropdown-item" href="/product/peach-yellow-hibiscus.php"><i class="fas fa-spa"></i> Peach Yellow Hibiscus</a></li>
-                            <li><a class="dropdown-item" href="/product/philodendron-birkin.php"><i class="fas fa-leaf"></i> Philodendron Birkin</a></li>
-                            <li><a class="dropdown-item" href="/product/pink-double-petal-hibiscus.php"><i class="fas fa-spa"></i> Pink Double Petal Hibiscus</a></li>
-                            <li><a class="dropdown-item" href="/product/snake-plant.php"><i class="fas fa-seedling"></i> Snake Plant</a></li>
-                        </ul>
-                    </li>
-                    <!-- <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">Plants</a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#categories"><i class="fas fa-apple-alt"></i> Fruit
-                                    Plants</a></li>
-                            <li><a class="dropdown-item" href="#categories"><i class="fas fa-spa"></i> Flowering
-                                    Plants</a></li>
-                            <li><a class="dropdown-item" href="#categories"><i class="fas fa-leaf"></i> Ornamental
-                                    Plants</a></li>
-                            <li><a class="dropdown-item" href="#categories"><i class="fas fa-mortar-pestle"></i>
-                                    Medicinal & Herbal</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#categories"><i class="fas fa-tree"></i> Farm &
-                                    Landscape</a></li>
-                        </ul>
-                    </li> -->
-<!--                    <li class="nav-item dropdown">-->
-<!--    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"-->
-<!--        aria-expanded="false">Plants</a>-->
-<!--    <ul class="dropdown-menu mega-plants w-100">-->
-<!--        <li><a class="dropdown-item" href="https://pavitramitti.co.in/product/aglaonema-snow-white"><i class="fas fa-leaf"></i> Aglaonema Snow White</a></li>-->
-<!--        <li><a class="dropdown-item" href="https://pavitramitti.co.in/product/aglaonema"><i class="fas fa-leaf"></i> Aglaonema</a></li>-->
-<!--        <li><a class="dropdown-item" href="https://pavitramitti.co.in/product/allamanda"><i class="fas fa-sun"></i> Allamanda</a></li>-->
-<!--        <li><a class="dropdown-item" href="https://pavitramitti.co.in/product/aloe-vera"><i class="fas fa-mortar-pestle"></i> Aloe Vera</a></li>-->
-<!--        <li><a class="dropdown-item" href="https://pavitramitti.co.in/product/bougainvillea"><i class="fas fa-spa"></i> Bougainvillea</a></li>-->
-<!--        <li><a class="dropdown-item" href="https://pavitramitti.co.in/product/dragon-fruit"><i class="fas fa-apple-alt"></i> Dragon Fruit</a></li>-->
-<!--        <li><a class="dropdown-item" href="https://pavitramitti.co.in/product/dragon-mulberry"><i class="fas fa-apple-alt"></i> Dragon Mulberry</a></li>-->
-<!--        <li><a class="dropdown-item" href="https://pavitramitti.co.in/product/exotic-peach-yellow-hibiscus"><i class="fas fa-spa"></i> Exotic Peach Yellow Hibiscus</a></li>-->
-<!--        <li><a class="dropdown-item" href="https://pavitramitti.co.in/product/frangipani"><i class="fas fa-spa"></i> Frangipani</a></li>-->
-<!--        <li><a class="dropdown-item" href="https://pavitramitti.co.in/product/grafted-hibiscus"><i class="fas fa-spa"></i> Grafted Hibiscus</a></li>-->
-<!--        <li><a class="dropdown-item" href="https://pavitramitti.co.in/product/hot-pink-hibiscus"><i class="fas fa-spa"></i> Hot Pink Hibiscus</a></li>-->
-<!--        <li><a class="dropdown-item" href="https://pavitramitti.co.in/product/jade-plant"><i class="fas fa-seedling"></i> Jade Plant</a></li>-->
-<!--        <li><a class="dropdown-item" href="https://pavitramitti.co.in/product/light-peach-hibiscus"><i class="fas fa-spa"></i> Light Peach Hibiscus</a></li>-->
-<!--        <li><a class="dropdown-item" href="https://pavitramitti.co.in/product/money-plant"><i class="fas fa-seedling"></i> Money Plant</a></li>-->
-<!--        <li><a class="dropdown-item" href="https://pavitramitti.co.in/product/orange-hibiscus"><i class="fas fa-spa"></i> Orange Hibiscus</a></li>-->
-<!--        <li><a class="dropdown-item" href="https://pavitramitti.co.in/product/peach-yellow-hibiscus"><i class="fas fa-spa"></i> Peach Yellow Hibiscus</a></li>-->
-<!--        <li><a class="dropdown-item" href="https://pavitramitti.co.in/product/philodendron-birkin"><i class="fas fa-leaf"></i> Philodendron Birkin</a></li>-->
-<!--        <li><a class="dropdown-item" href="https://pavitramitti.co.in/product/pink-double-petal-hibiscus"><i class="fas fa-spa"></i> Pink Double Petal Hibiscus</a></li>-->
-<!--        <li><a class="dropdown-item" href="https://pavitramitti.co.in/product/snake-plant"><i class="fas fa-seedling"></i> Snake Plant</a></li>-->
-<!--    </ul>-->
-<!--</li>-->
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">Services</a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="/services/nursery-consultation.html"><i
-                                        class="fas fa-seedling"></i> Nursery
-                                    Consultation</a></li>
-                            <li><a class="dropdown-item" href="/services/bulk-orders.html"><i class="fas fa-truck"></i>
-                                    Bulk Orders &
-                                    Delivery</a></li>
-                            <li><a class="dropdown-item" href="/services/landscaping-support.html"><i
-                                        class="fas fa-drafting-compass"></i>
-                                    Landscaping Support</a></li>
-                            <li><a class="dropdown-item" href="/services/plant-care-guidance.html"><i
-                                        class="fas fa-hand-holding-heart"></i> Plant
-                                    Care Guidance</a></li>
-                            <li><a class="dropdown-item" href="/services/soil-fertility-consultation.html"><i
-                                        class="fas fa-hand-holding-heart"></i> Soil Fertility Consultation</a></li>
-                        </ul>
-                    </li>
-                    <!-- <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#faq">FAQ</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li> -->
-                    <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#faq">FAQ</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
-                </ul>
-
-                <div class="nav-ctas ms-lg-2">
-                    <a href="tel:+919876543210" class="nav-btn nav-btn-phone">
-                        <i class="fas fa-phone-alt"></i> Call Us
-                    </a>
-                    <a href="https://wa.me/919876543210" target="_blank" class="nav-btn nav-btn-wa">
-                        <i class="fab fa-whatsapp"></i> WhatsApp
-                    </a>
-                </div>
+            <div class="nav-ctas ms-lg-2">
+                <a href="tel:+918250158613" class="nav-btn nav-btn-phone">
+                    <i class="fas fa-phone-alt"></i> Call Us
+                </a>
+                <a href="https://wa.me/918250158613" target="_blank" class="nav-btn nav-btn-wa">
+                    <i class="fab fa-whatsapp"></i> WhatsApp
+                </a>
             </div>
         </div>
-    </nav>
+    </div>
+</nav>
