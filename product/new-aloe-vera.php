d2c0c23b3e7c7bcf12f35ed2f04cf9495404dc28
AmanPrasad <amanprasad.um@gmail.com>
2026-04-25 12:15:54 +0530

    sdf

--- product/new-aloe-vera.php
+++ product/new-aloe-vera.php
@@ -6,7 +6,7 @@
   <title>Aloe Vera Live Plant</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"/>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css"/>
-  <link rel="stylesheet" href="../assets/css/product-details.css">
+  <link rel="stylesheet" href="../assets/css/new-product-details.css"> 
   <?php include '../common/header-script.php'; ?>
 </head>
 <body>
