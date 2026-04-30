8d3fde08ae9c255942b64020e14b6f0b81ed1f0e
amanprasadum <amanprasad.um@gmail.com>
2026-05-01 00:35:51 +0530

    sdf

--- common/header-script.php
+++ common/header-script.php
@@ -8,6 +8,7 @@
     <!-- AOS CSS -->
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <!-- Google Fonts -->
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link
         href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=DM+Sans:wght@300;400;500;600;700&family=Caveat:wght@600&display=swap"
         rel="stylesheet">
