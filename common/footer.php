d38848ff0edc1d5de5d9596e94cad0620daad1ce
AmanPrasad <amanprasad.um@gmail.com>
2026-05-02 16:01:18 +0530

    sdf

--- common/footer.php
+++ common/footer.php
@@ -22,10 +22,7 @@
                 <i class="fas fa-envelope"></i>
                 <div>
                     <strong>Email Us</strong>
-                    <span>info@pavitramitti.com
-                    </span>
-                    <!-- <span><a href="/cdn-cgi/l/email-protection" class="__cf_email__"
-                            data-cfemail="1e777078715e6e7f68776a6c7f73776a6a77307770">[email&#160;protected]</a></span> -->
+                    <span>info@pavitramitti.com</span>
                 </div>
             </div>
             <div class="ftb-item">
@@ -49,23 +46,19 @@
                     over a decade.</p>
 
                 <div class="social-row">
-                    <a href="https://www.facebook.com/PavitraMitti/" target="_blank" class="soc-icon"
-                        aria-label="Facebook">
+                    <a href="https://www.facebook.com/PavitraMitti/" target="_blank" class="soc-icon" aria-label="Facebook">
                         <i class="fab fa-facebook-f"></i>
                     </a>
                     <a href="https://x.com/PavitraMitti" target="_blank" class="soc-icon" aria-label="Twitter">
                         <i class="fab fa-x-twitter"></i>
                     </a>
-                    <a href="https://in.pinterest.com/pavitramitti/" target="_blank" class="soc-icon"
-                        aria-label="Pinterest">
+                    <a href="https://in.pinterest.com/pavitramitti/" target="_blank" class="soc-icon" aria-label="Pinterest">
                         <i class="fab fa-pinterest"></i>
                     </a>
-                    <a href="https://www.youtube.com/@PavitraMitti" target="_blank" class="soc-icon"
-                        aria-label="YouTube">
+                    <a href="https://www.youtube.com/@PavitraMitti" target="_blank" class="soc-icon" aria-label="YouTube">
                         <i class="fab fa-youtube"></i>
                     </a>
-                    <a href="https://in.linkedin.com/company/pavitramitti" target="_blank" class="soc-icon"
-                        aria-label="LinkedIn">
+                    <a href="https://in.linkedin.com/company/pavitramitti" target="_blank" class="soc-icon" aria-label="LinkedIn">
                         <i class="fab fa-linkedin-in"></i>
                     </a>
                     <a href="https://wa.me/918250158613" target="_blank" class="soc-icon" aria-label="WhatsApp">
@@ -74,57 +67,45 @@
                 </div>
             </div>
 
-            <!-- Quick Links -->
+            <!-- Our Plants -->
             <div class="col-lg-2 col-md-6 mb-4" data-aos="fade-up" data-aos-delay="100">
-                <h5 class="foot-h">Quick Links</h5>
+                <h5 class="foot-h">Our Plants</h5>
                 <ul class="foot-links">
-                    <li><a href="#categories"><i class="fas fa-chevron-right"></i> Our Plants</a></li>
-                    <li><a href="#shop"><i class="fas fa-chevron-right"></i> Popular Plants</a></li>
-                    <li><a href="/#about"><i class="fas fa-chevron-right"></i> About Us</a></li>
-                    <li><a href="/#faq"><i class="fas fa-chevron-right"></i> FAQ</a></li>
-                    <li><a href="/#contact"><i class="fas fa-chevron-right"></i> Contact Us</a></li>
+                    <li><a href="/product/aglaonema-snow-white"><i class="fas fa-chevron-right"></i> Aglaonema Snow White</a></li>
+                    <li><a href="/product/aglaonema"><i class="fas fa-chevron-right"></i> Aglaonema</a></li>
+                    <li><a href="/product/allamanda"><i class="fas fa-chevron-right"></i> Allamanda</a></li>
+                    <li><a href="/product/aloe-vera"><i class="fas fa-chevron-right"></i> Aloe Vera</a></li>
+                    <li><a href="/product/bougainvillea"><i class="fas fa-chevron-right"></i> Bougainvillea</a></li>
+                    <li><a href="/product/dragon-fruit"><i class="fas fa-chevron-right"></i> Dragon Fruit</a></li>
+                    <li><a href="/product/dragon-mulberry"><i class="fas fa-chevron-right"></i> Dragon Mulberry</a></li>
                 </ul>
             </div>
 
-            <!-- Plant Categories -->
+            <!-- Popular Plants -->
             <div class="col-lg-3 col-md-6 mb-4" data-aos="fade-up" data-aos-delay="200">
-                <h5 class="foot-h">Our Plants</h5>
+                <h5 class="foot-h">Popular Plants</h5>
                 <ul class="foot-links">
-                    <li><a href="/product/dragon-fruit"><i class="fas fa-chevron-right"></i> Fruit Plants</a></li>
-                    <li><a href="/product/bougainvillea"><i class="fas fa-chevron-right"></i> Flowering Plants</a></li>
-                    <li><a href="/product/aglaonema"><i class="fas fa-chevron-right"></i> Ornamental Plants</a></li>
-                    <li><a href="/product/aloe-vera"><i class="fas fa-chevron-right"></i> Medicinal & Herbal</a></li>
-                    <li><a href="/product/index"><i class="fas fa-chevron-right"></i> Farm & Landscape</a></li>
+                    <li><a href="/product/frangipani"><i class="fas fa-chevron-right"></i> Frangipani</a></li>
+                    <li><a href="/product/jade-plant"><i class="fas fa-chevron-right"></i> Jade Plant</a></li>
+                    <li><a href="/product/money-plant"><i class="fas fa-chevron-right"></i> Money Plant</a></li>
+                    <li><a href="/product/philodendron-birkin"><i class="fas fa-chevron-right"></i> Philodendron Birkin</a></li>
+                    <li><a href="/product/snake-plant"><i class="fas fa-chevron-right"></i> Snake Plant</a></li>
+                    <li><a href="/product/hot-pink-hibiscus"><i class="fas fa-chevron-right"></i> Hibiscus Hot Pink</a></li>
+                    <li><a href="/product/grafted-hibiscus"><i class="fas fa-chevron-right"></i> Grafted Hibiscus</a></li>
                 </ul>
             </div>
 
             <!-- Contact Form -->
             <div class="col-lg-4 col-md-6 mb-4" data-aos="fade-up" data-aos-delay="300">
                 <h5 class="foot-h">Get In Touch</h5>
-                <!-- <form class="foot-form" id="footerForm">
-                    <input type="text" class="fc" placeholder="Your Name" required>
-                    <input type="email" class="fc" placeholder="Your Email" required>
-                    <textarea class="fc" placeholder="Your Message" required></textarea>
+                <form class="foot-form" id="footerForm" action="https://api.web3forms.com/submit" method="POST">
+                    <input type="hidden" name="access_key" value="cf185d87-1b86-4377-88d6-6ff2e0fce031">
+                    <input type="hidden" name="subject" value="New Footer Contact Message">
+                    <input type="text" name="name" class="fc" placeholder="Your Name" required>
+                    <input type="email" name="email" class="fc" placeholder="Your Email" required>
+                    <textarea name="message" class="fc" placeholder="Your Message" required></textarea>
                     <button type="submit" class="foot-submit">
                         <i class="fas fa-paper-plane"></i> Send Message
-                    </button>
-                </form> -->
-                <form class="foot-form" id="footerForm" action="https://api.web3forms.com/submit" method="POST">
-
-                    <!-- Web3Forms Key -->
-                    <input type="hidden" name="access_key" value="cf185d87-1b86-4377-88d6-6ff2e0fce031">
-
-                    <!-- Optional -->
-                    <input type="hidden" name="subject" value="New Footer Contact Message">
-
-                    <input type="text" name="name" class="fc" placeholder="Your Name" required>
-
-                    <input type="email" name="email" class="fc" placeholder="Your Email" required>
-
-                    <textarea name="message" class="fc" placeholder="Your Message" required></textarea>
-
-                    <button type="submit" class="foot-submit">
-                        <i class="fas fa-paper-plane"></i> Send Message cx
                     </button>
                 </form>
             </div>
@@ -175,9 +156,6 @@
                 <p class="foot-copy mb-0">
                     &copy; 2026 Pavitra Mitti Nursery, All Rights Reserved.
                 </p>
-                <!--  <p class="foot-design mb-0">
-                    Designed & Maintained by <a href="https://webeasyexperts.com" target="_blank">Web Easy Experts</a>
-                </p> -->
                 <div class="foot-bottom-links">
                     <a href="#">Privacy Policy</a>
                     <a href="#">Terms of Use</a>
